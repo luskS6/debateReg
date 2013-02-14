91226799c05e95c4f34caebb64fb7a38d8f315db
LuskS6 <lusk.pbj@gmail.com>
2013-02-14 04:52:50 -0500

    wpi-reg name change

--- app/mailers/school_mailer.rb
+++ app/mailers/school_mailer.rb
@@ -5,7 +5,7 @@ ActionMailer::Base.smtp_settings = {
   :address        => ENV['MAILGUN_SMTP_SERVER'],
   :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
   :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-  :domain         => 'floating-meadow-8628.heroku.com',
+  :domain         => 'wpi-reg.heroku.com',
   :authentication => :plain,
 }
 ActionMailer::Base.delivery_method = :smtp
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,7 +58,7 @@ DebateReg::Application.configure do
   # the I18n.default_locale when a translation can not be found)
   config.i18n.fallbacks = true
   config.assets.precompile += %w[*.png *.jpg *.jpeg *.gif] 
-  config.action_mailer.default_url_options = { :host => 'floating-meadow-8628.heroku.com' }
+  config.action_mailer.default_url_options = { :host => 'wpi-reg.heroku.com' }
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
